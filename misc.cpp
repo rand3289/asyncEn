cd3e01d3da6c90a630883995b4ab812fbef25424
rand3289 <rand3289@rand3289.com>
2025-11-13 23:37:04 -0500

    tweaking drawCircle()

--- misc.cpp
+++ misc.cpp
@@ -2,14 +2,10 @@
 #include <SDL2/SDL.h> // Simple Directmedia Layer lib has to be installed
 
 
-// https://discourse.libsdl.org/t/query-how-do-you-draw-a-circle-in-sdl2-sdl2/33379
 void drawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_t radius){
-    const int32_t diameter = (radius * 2);
-    int32_t x = (radius - 1);
+    int32_t x = radius;
     int32_t y = 0;
-    int32_t tx = 1;
-    int32_t ty = 1;
-    int32_t error = (tx - diameter);
+    int32_t err = 0;
 
     while (x >= y){
         // Each of the following renders an octant of the circle
@@ -22,15 +18,13 @@ void drawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_
         SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
         SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
 
-        if (error <= 0){
+        if (err <= 0){
   	        ++y;
-  	        error += ty;
-  	        ty += 2;
+  	        err += 2*y+1;
         }
-        if (error > 0){
+        if (err > 0){
   	        --x;
-  	        tx += 2;
-  	        error += (tx - diameter);
+  	        err -= 2*x+1;
         }
     }
 }
